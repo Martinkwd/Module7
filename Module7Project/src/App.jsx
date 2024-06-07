e409d491b018753fe6df1f3092eb37fbc1ca6743
Martinkwd <martin.romero.carrasco@gmail.com>
2024-06-07 16:58:06 +1200

    module 7

---
+++ Module7Project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import BitcoinRates from "./bitcoinRates";
+
+function App() {
+  return (
+    <>
+      <BitcoinRates></BitcoinRates>
+    </>
+  );
+}
+
+export default App;
